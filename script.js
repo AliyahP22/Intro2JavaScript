3dc9474289b50d74f82dfda88665c846262a87ca
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-03-12 18:02:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction(){
+document.getElementById("greet").style.color="blue"
+}
+function night(){ 
+  document.body.style.backgroundColor="darkblue";
+}
+function myFunction2(){
+document.body.style.fontFamily="Cursive"; document.body.style.color="white";
+}
